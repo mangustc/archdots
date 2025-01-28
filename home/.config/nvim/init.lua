7545b75bb474ea34bd8a8f4b8ce76b029965d4a7
Ivan Lifanov <letalbark@gmail.com>
2025-01-28 22:06:11 +0700

    update neovim configuration

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,13 +1,3 @@
-if vim.g.neovide then
-	vim.g.neovide_padding_top = 20
-	vim.g.neovide_padding_bottom = 20
-	vim.g.neovide_padding_right = 20
-	vim.g.neovide_padding_left = 20
-	vim.g.neovide_position_animation_length = 0.05
-	vim.g.neovide_scroll_animation_length = 0.05
-	vim.g.neovide_cursor_animation_length = 0.05
-end
-
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -20,7 +10,7 @@ vim.opt.showmode = false
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 
-vim.opt.clipboard = "unnamedplus"
+-- vim.opt.clipboard = "unnamedplus"
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.local/share/nvim/undodir"
@@ -64,10 +54,11 @@ end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{
-		"nmac427/guess-indent.nvim",
-		opts = {},
-	},
+	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+	-- {
+	-- 	"nmac427/guess-indent.nvim",
+	-- 	opts = {},
+	-- },
 	{ "numToStr/Comment.nvim", opts = {} },
 	{
 		"nvim-telescope/telescope.nvim",
@@ -112,13 +103,25 @@ require("lazy").setup({
 		end,
 	},
 	{
+		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+		-- used for completion, annotations and signatures of Neovim apis
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {
+			library = {
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
 			{ "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
 			"williamboman/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
 			{ "j-hui/fidget.nvim", opts = {} },
-			{ "folke/neodev.nvim", opts = {} },
+			"hrsh7th/cmp-nvim-lsp",
 		},
 		config = function()
 			vim.api.nvim_create_autocmd("LspAttach", {
@@ -326,27 +329,44 @@ require("lazy").setup({
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = { signs = false },
 	},
-	{
+	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-context",
-		},
 		build = ":TSUpdate",
+		main = "nvim-treesitter.configs", -- Sets main module to use for opts
+		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 		opts = {
-			ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
+			ensure_installed = {
+				"bash",
+				"c",
+				"diff",
+				"html",
+				"lua",
+				"luadoc",
+				"markdown",
+				"markdown_inline",
+				"query",
+				"vim",
+				"vimdoc",
+			},
+			-- Autoinstall languages that are not installed
 			auto_install = true,
 			highlight = {
 				enable = true,
+				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+				--  If you are experiencing weird indenting issues, add the language to
+				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
 				additional_vim_regex_highlighting = { "ruby" },
 			},
 			indent = { enable = true, disable = { "ruby" } },
 		},
-		config = function(_, opts)
-			require("nvim-treesitter.install").prefer_git = true
-			---@diagnostic disable-next-line: missing-fields
-			require("nvim-treesitter.configs").setup(opts)
-		end,
+		-- There are additional nvim-treesitter modules that you can use to interact
+		-- with nvim-treesitter. You should go explore a few and see what interests you:
+		--
+		--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 	},
+
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		version = "*",
@@ -357,7 +377,7 @@ require("lazy").setup({
 		},
 		cmd = "Neotree",
 		keys = {
-			{ "<leader>pt", ":Neotree reveal<CR>", { desc = "NeoTree reveal" } },
+			{ "<leader>pt", ":Neotree reveal<CR>", { desc = "NeoTree reveal", silent = true } },
 		},
 		opts = {
 			filesystem = {
@@ -388,15 +408,42 @@ require("lazy").setup({
 			-- indent = { char = '»' },
 		},
 	},
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		opts = {
-			options = {
-				icons_enabled = true,
-				theme = "catppuccin",
-			},
-		},
+	{ -- Collection of various small independent plugins/modules
+		"echasnovski/mini.nvim",
+		config = function()
+			-- Better Around/Inside textobjects
+			--
+			-- Examples:
+			--  - va)  - [V]isually select [A]round [)]paren
+			--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+			--  - ci'  - [C]hange [I]nside [']quote
+			require("mini.ai").setup({ n_lines = 500 })
+
+			-- Add/delete/replace surroundings (brackets, quotes, etc.)
+			--
+			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+			-- - sd'   - [S]urround [D]elete [']quotes
+			-- - sr)'  - [S]urround [R]eplace [)] [']
+			require("mini.surround").setup()
+
+			-- Simple and easy statusline.
+			--  You could remove this setup call if you don't like it,
+			--  and try some other statusline plugin
+			local statusline = require("mini.statusline")
+			-- set use_icons to true if you have a Nerd Font
+			statusline.setup({ use_icons = vim.g.have_nerd_font })
+
+			-- You can configure sections in the statusline by overriding their
+			-- default behavior. For example, here we set the section for
+			-- cursor location to LINE:COLUMN
+			---@diagnostic disable-next-line: duplicate-set-field
+			statusline.section_location = function()
+				return "%2l:%-2v"
+			end
+
+			-- ... and there is more!
+			--  Check out: https://github.com/echasnovski/mini.nvim
+		end,
 	},
 }, {
 	ui = {
